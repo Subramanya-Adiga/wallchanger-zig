29805bf533e5f8286b32f61314d4c06465e09e95
Subramanya <subramanya.adiga4@protonmail.com>
2025-07-02 20:13:42 +0530

    Added Changer Module To Build. Added Path Table

---
+++ src/lib/path_table.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+const Self = @This();
+
+allocator: std.mem.Allocator = undefined,
+arena: std.heap.ArenaAllocator = undefined,
+path_store: std.ArrayList(PathType) = undefined,
+modified: bool = false,
+
+const PathType = struct { id: u32, path: []const u8 };
+
+pub fn init(allocator: std.mem.Allocator) Self {
+    var ret: Self = .{
+        .allocator = allocator,
+        .arena = std.heap.ArenaAllocator.init(allocator),
+    };
+    ret.path_store = std.ArrayList(PathType).init(ret.arena.allocator());
+    return ret;
+}
+
+pub fn deinit(self: *Self) void {
+    _ = self.arena.reset(.free_all);
+    self.arena.deinit();
+    self.modified = false;
+}
+
+pub fn insert(self: *Self, path: []const u8) !bool {
+    if (std.fs.path.dirname(path)) |has_path| {
+        const crc = std.hash.Crc32.hash(has_path);
+        const append_type: PathType = .{ .id = crc, .path = path };
+        if (std.mem.containsAtLeast(PathType, self.path_store.items, 0, append_type)) {
+            try self.path_store.append(append_type);
+            return true;
+        }
+        return false;
+    }
+    return error.NullPath;
+}
+
+pub fn get(self: *Self, id: u32) ?[]const u8 {
+    for (self.path_store.items) |ent| {
+        if (ent.id == id) {
+            return ent.path;
+        }
+    }
+    return null;
+}
---
+++ src/lib/root.zig
@@ -0,0 +1,1 @@
+pub const PathTable = @import("path_table.zig");
