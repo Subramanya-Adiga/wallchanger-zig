19ac7b98d188a4e1b2ae3503e410989dd38562aa
Subramanya <subramanya.adiga4@protonmail.com>
2025-11-01 22:31:43 +0530

    Added Testing To Changer Module

--- src/lib/root.zig
+++ src/lib/root.zig
@@ -1,3 +1,9 @@
+const std = @import("std");
+
 pub const StrTable = @import("string_table.zig");
 pub const Cache = @import("cache.zig");
 pub const CacheLibrary = @import("cache_library.zig");
+
+test {
+    std.testing.refAllDecls(@This());
+}
